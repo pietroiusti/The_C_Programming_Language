5c01595ed5f8fed376e66c248cbbcf4a9f68f413
Giulio <giulio.pietroiusti@gmail.com>
2016-07-26 13:42:56 +0200

    1-1, 1-2 rev

--- 1/1-2.c
+++ 1/1-2.c
@@ -2,6 +2,42 @@
 
 int main()
 {
-	printf("\K\n");
-}
+	printf("Alert (bell) character, \\a:\a");
+    printf("\n");
 
+    printf("Backspace, \\b:\b");
+    printf("\n");
+
+    printf("Formfeed, \\f:\f");
+    printf("\n");
+
+    printf("Newline, \\n:\n");
+    printf("\n");
+
+    printf("Carriage return, \\r:\r");
+    printf("\n");
+
+    printf("Horizontal tab, \\t:\t");
+    printf("\n");
+
+    printf("Verical tab, \\v:\v");
+    printf("\n");
+
+    printf("Backslash, \\\\: \\");
+    printf("\n");
+
+    printf("Question mark, \\?: \?");
+    printf("\n");
+
+    printf("Single quote, \\': \'");
+    printf("\n");
+
+    printf("Double quote, \\\": \"");
+    printf("\n");
+
+    printf("Octal number, \\ooo");
+    printf("\n");
+
+    printf("Hexadecimal number, \\xhh");
+    printf("\n");
+}
