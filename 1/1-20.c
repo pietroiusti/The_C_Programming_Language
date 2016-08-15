08e8a4b1434205e08cab8a0533254be80475dfe4
Giulio <giulio@localhost.localdomain>
2016-08-15 20:38:56 +0200

    properly handles tab at the beginning

--- 1/1-20.c
+++ 1/1-20.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #define MAXSTRING 1000
+#define TABSTOPS 8
+
+/* write a program detab that replaces tabs in the input
+ * with the proper number of blanks to space to the next
+ * tab stop. Assume a fixed set of tab stop, say every n
+ * columns. Should n be a variable or a symbolic parameter?
+ */
+
+/* n should be a symbolic parameter since its value will
+ * never change through the program
+ */
 
 int main(void)
 {
-    int n = 3;
     int c, i;
     char input[MAXSTRING];
 
     for (i = 0; (c = getchar()) != EOF; i++) {
-        if (c == '\t') {
-            while (i % n != 0) {
+        if (c == '\t') { // if the input char is a tab
+            if (i == 0) { // if the tab is at the beginning we handle it this way...
+                input[i] = ' ';
+                ++i;
+            }
+            while (i % TABSTOPS != 0) { //condition: till we don't get a multiple of TABSTOPS
                 input[i] = ' ';
                 ++i;
             }
             i--;
         } 
-        else {
+        else { // if the input char is not a tab
             input[i] = c;
         }
     }
