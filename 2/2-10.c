1b2d0e751917fec0629596969f654ea2f0609f69
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-08-20 23:26:59 +0200

    Add instructions

--- 2/2-10.c
+++ 2/2-10.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/* Rewrite the function lower, which converts upper case letters to
+ * lower case letters, with a conditional expression instead of
+ * if-else.
+ */
+
 int lower(int c)
 {
    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c; 
--- 2/2-6.c
+++ 2/2-6.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/* Write a function setbits(x,p,n,y) that returns x with the n bits
+ * that begin at position p set to the rightmost n bits of y, leaving
+ * the other bits unchanged.
+ */
+
 unsigned getbits(unsigned x, int p, int n);
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 
