23a741897c0081b7ca1b9b66f52e4a9f5544fc3f
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2021-01-07 00:11:36 +0100

    Fix 1-13.

--- 1/1-13.c
+++ 1/1-13.c
@@ -3,56 +3,47 @@
 #define IN    1    /* inside a word */
 #define OUT   0    /* outside a word */
 
-/* write a program to print a histogram of
- * the lengths of words in its input. It 
- * easy to draw a the histogram with the bars
- * horizontal; a vertical orientation is more
- * challenging.
+/* write a program to print a histogram of the lengths of words in its
+ * input. It easy to draw a the histogram with the bars horizontal; a
+ * vertical orientation is more challenging.
  */
 
 
 /* Horizontal histogram version. */
 
-int main(void)
+main()
 {
-    /*array of words lengths*/
-    int lengths[100];
+    int c, i, j, nw, state;
 
-    int i, j;
-    for (i = 0; i < 100; i++) {
-        lengths[i] = 0;
-    }
+    int current_word_length = 0;
+    int word_lengths[100];
 
-    int c, length, wordcount, state;
+    for (i = 0; i < 100; ++i)
+	word_lengths[i] = 0;
+
     state = OUT;
-    wordcount = length = 0;
-    
+    nw = 0;
     while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\n' || c == '\t') {
-            if (state == IN){
-                lengths[wordcount] = length;        
-                length = 0;
-                ++wordcount;
-            }
-            state = OUT;
-        }
-        else{
-            state = IN;
-            ++length;
-        }
+	if (c == ' ' || c == '\n' || c == '\t') {
+	    if (state == IN) {
+		state = OUT;
+		word_lengths[nw-1] = current_word_length;
+		current_word_length = 0;
+	    }
+	}
+	else if (state == OUT) {
+	    state = IN;
+	    ++nw;
+	    ++current_word_length;
+	}
+	else if (state == IN)
+	    ++current_word_length;
     }
 
-    /*print arrays of lengths. not necessary*/
-    for (i = 0; i < 5; i++) 
-        printf("%d ", lengths[i]);
-    printf("\n");
-
-    /*print horizontal histogram*/
-    for (i = 0; i < wordcount; i++) {
-        for (j = 0; j < lengths[i]; j++)
-            putchar('#');
-        putchar('\n');
+    for (i = 0; i < nw; ++i) {
+	for (j = 0; j < word_lengths[i]; ++j) {
+	    printf("#");
+	}
+	printf("\n");
     }
-
-    return 0;
 }
