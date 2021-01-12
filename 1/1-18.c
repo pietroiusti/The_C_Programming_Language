664b5693ee7e5df8f454706f1d7aace6b944234e
glptrst <giulio.pietroiusti@gmail.com>
2021-01-12 23:48:08 +0100

    Fix 1-18.

--- 1/1-18.c
+++ 1/1-18.c
@@ -1,77 +1,47 @@
 #include <stdio.h>
-#define MAXBLANKS 100
-#define MAXSTRING 1000
 
-void removeblanklines(char s[]);
+#define MAXLINE 1000
+#define MAXTEXT 10000
 
-int main(void)
+/* Write a program to remove trailing blanks and tabs from each line
+ * of input, and to delete entirely blank lines */
+
+void clean_and_copy_input(char to[]);
+
+main()
 {
-    char string[MAXSTRING];
-    int c, c2, c3;
-    int j = 0;
+    char text[MAXTEXT];
 
-    while ((c = getchar()) != EOF) {
-        if (c != ' ' && c != '\t') {
-            //putchar(c);
-            string[j] = c;
-            ++j;
-        }
-        else { 
-            int i;
-            char temp[MAXBLANKS]; 
-            if (c == ' ')
-                temp[0] = ' ';
-            else
-                temp[0] = '\t';
+    int c, i;
 
-            for (i = 1; (c2 = getchar()) == ' ' || c2 == '\t'; ++i)
-                temp[i] = c2;
-            if (c2 != '\n') { //if they are not trailing
-                temp[i] = c2;
-                ++i;
-                temp[i] = '\0';
-                //printf("%s", temp);
-                for (i = 0; temp[i] != '\0'; ++i, ++j) {
-                    string[j] = temp[i];    
-                }
-            }
-            else {//if they are trailing
-               //putchar('\n'); 
-               string[j] = '\n';
-               ++j;
-            }
-        }
-    }
-    string[j] = '\0';
+    clean_and_copy_input(text);
 
-    removeblanklines(string);
+    printf(text);
 
-    printf("%s", string);
-
-    return 0;
 }
 
-void removeblanklines(char s[])
+/* Remove trailing blanks and tabs and blank lines from input and copy
+ * it into `to` */
+void clean_and_copy_input(char to[])
 {
-    char temp[1000];
-
-    int i, j;
-
-    for (i = 0, j = 0; s[i] != '\0'; ++i, ++j) {
-        if ( s[i] != '\n')
-            temp[j] = s[i];
-        else {
-            temp[j] = '\n';
-            while (s[i] == '\n')
-                ++i;
-            ++j;
-            temp[j] = s[i];
-        }
+    int c, i;
+    int previous = '\n';
+    
+    for (i = 0; (c = getchar()) != EOF && i < MAXTEXT; ++i) {
+	if (c == '\t' && previous == '\t') {
+	    --i;
+	}
+	else if (c == ' ' && previous == ' ') {
+	    --i;
+	}
+	else if (c == '\n' && previous == '\n') {
+	    --i;
+	}
+	else {
+	    to[i] = c;
+	    previous = c;
+	}
     }
-    temp[i] = '\0';
-
-    for (i = 0; temp[i] != '\0'; ++i) {
-        s[i] = temp[i];
-    }
-    s[i] = '\0';
+    to[i] = '\0';
 }
+
