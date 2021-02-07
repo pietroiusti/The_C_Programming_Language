a0774233de1ee051a726136dc1c9f9560f53a51d
glptrst <giulio.pietroiusti@gmail.com>
2021-02-07 22:34:23 +0100

    Fix 2-6.

--- 2/2-6.c
+++ 2/2-6.c
@@ -5,31 +5,27 @@
  * the other bits unchanged.
  */
 
-unsigned getbits(unsigned x, int p, int n);
 unsigned setbits(unsigned x, int p, int n, unsigned y);
 
 int main(void)
 {
-    //printf("~(getbits(~0, 3, 4) << 3) = %x\n", ~(getbits(~0, 3, 4) << 3));
-
-    //printf("getbits(0x79, 3, 4) << 3 = %x\n", getbits(0x79, 3, 4) << 3);
-
-    printf("%x\n", setbits(0xa2, 5, 3, 0xf3));
-
-    return 0;
+    printf("%i\n", setbits(63, 5, 3, 2));
 }
 
-/* getbits: get n bits from position p */
-unsigned getbits(unsigned x, int p, int n)
-{
-    return (x >> (p+1-n)) & ~(~0 << n);
-}
-
-/* setbits: return x with the n bits that begin at position p
- * set to the rightmost n bits of y, leaving the other bits
- * unchanged.
- */
 unsigned setbits(unsigned x, int p, int n, unsigned y)
 {
-    return (x & ( ~ (getbits(~0, n-1, n) << (p+1-n)))) | (getbits(y, n-1, n) << (p+1-n));
+    unsigned result;
+    int shift = p-n+1;
+    
+    unsigned bits = y & (1<<n)-1; /* get the n rightmost bits of y */
+
+    bits <<= shift; /* shift bits the right amount */
+
+    // turn bits on
+    result = x | bits;
+
+    //turn bits off
+    result = result & ~((~y & ((1<<n)-1) ) << shift);
+            
+    return result;
 }
